ee7c535a3105fa81c669173e93b5fa74244629e5
lucasgomes14 <lucazmatehus14@gmail.com>
2024-11-20 13:18:20 -0300

    Post model

---
+++ AcademicHub/src/main/java/com/academichub/AcademicHub/model/Post.java
@@ -0,0 +1,56 @@
+package com.academichub.AcademicHub.model;
+
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+@Table(name = "post")
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "description", nullable = false)
+    private String description;
+
+    @OneToMany(mappedBy = "post", cascade = CascadeType.ALL)
+    private List<ContentPost> content;
+
+    @OneToMany
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<ContentPost> getContent() {
+        return content;
+    }
+
+    public void setContent(List<ContentPost> content) {
+        this.content = content;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
