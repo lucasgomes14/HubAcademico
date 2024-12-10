85a28c18e5b2bf09d237e9a8b4a21efa9d0ef9a1
Isaac Alves Leal de Oliveira <isaacalves2406@gmail.com>
2024-12-10 15:05:50 -0300

    Dashboard incompleto, ja com tudo da master

--- AcademicHub/src/main/java/com/academichub/AcademicHub/controller/AuthenticationController.java
+++ AcademicHub/src/main/java/com/academichub/AcademicHub/controller/AuthenticationController.java
@@ -12,7 +12,6 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
-import java.time.LocalDateTime;
 
 @RestController
 @RequestMapping("/auth")
