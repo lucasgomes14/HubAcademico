a6266b7f2f05aa81b54fab24ab66dfe8c1132254
lucasgomes14 <lucazmatehus14@gmail.com>
2024-12-11 14:57:12 -0300

    username validate

--- AcademicHub/src/main/java/com/academichub/AcademicHub/service/UserProfileService.java
+++ AcademicHub/src/main/java/com/academichub/AcademicHub/service/UserProfileService.java
@@ -1,11 +1,14 @@
 package com.academichub.AcademicHub.service;
 
 import com.academichub.AcademicHub.dto.UserProfileResponseDTO;
+import com.academichub.AcademicHub.exceptions.EmptyUsernameException;
 import com.academichub.AcademicHub.exceptions.UserNotFoundException;
 import com.academichub.AcademicHub.model.user.User;
 import com.academichub.AcademicHub.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.function.EntityResponse;
 
 @Service
 @RequiredArgsConstructor
@@ -14,10 +17,18 @@ public class UserProfileService {
     private final UserRepository userRepository;
 
     public UserProfileResponseDTO getUserProfile(String username) {
+        validateUsername(username);
+
         User user = userRepository.findByUsername(username).orElseThrow(UserNotFoundException::new);
 
         return new UserProfileResponseDTO(user.getName(), user.getLastName(),
                 user.getUsername(), user.getDateAndTimeOfUserCreation(), user.getUserUpdateDateAndTime(),
                 user.getEmail(), user.getRole(), user.getCourse(), user.getPosts());
+    }
+
+    private void validateUsername(String username) {
+        if (username.trim().isEmpty()) {
+            throw new EmptyUsernameException();
+        }
     }
 }
