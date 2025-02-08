f41534405139644db723890e1ddbf015f86703e9
0xPedrop <paulo.pedro@academico.ifpb.edu.br>
2025-02-08 20:46:25 -0300

    test

--- AcademicHub/src/main/java/Front/src/app/pages/login/login.component.ts
+++ AcademicHub/src/main/java/Front/src/app/pages/login/login.component.ts
@@ -1,14 +1,20 @@
 import { Component } from '@angular/core';
 import { DefaultLoginLayoutComponent } from '../../components/default-login-layout/default-login-layout.component';
-import { FormControl, FormGroup, FormRecord, ReactiveFormsModule, Validators } from '@angular/forms';
+import {
+  FormControl,
+  FormGroup,
+  FormRecord,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { PrimaryInputComponent } from '../../components/primary-input/primary-input.component';
 import { Router } from '@angular/router';
 import { LoginService } from '../../services/login.service';
 import { ToastrService } from 'ngx-toastr';
 
 interface LoginForm {
-  email: FormControl,
-  password: FormControl
+  email: FormControl;
+  password: FormControl;
 }
 
 @Component({
@@ -17,37 +23,46 @@ interface LoginForm {
   imports: [
     DefaultLoginLayoutComponent,
     ReactiveFormsModule,
-    PrimaryInputComponent
+    PrimaryInputComponent,
   ],
-  providers: [
-    LoginService
-  ],
+  providers: [LoginService],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
 })
 export class LoginComponent {
-  //começa vazio e depois é declarado 
+  //começa vazio e depois é declarado
   loginForm!: FormGroup<LoginForm>;
   //construção do formulário
   constructor(
     private router: Router,
     private loginService: LoginService,
     private toastService: ToastrService
-  ){
+  ) {
     this.loginForm = new FormGroup({
       email: new FormControl('', [Validators.required, Validators.email]),
-      password: new FormControl('', [Validators.required, Validators.minLength(6)])
-    })
+      password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(6),
+      ]),
+    });
   }
 
-  submit(){
-    this.loginService.login(this.loginForm.value.email, this.loginForm.value.password).subscribe({
-      next: () => this.toastService.success("Login feito com sucesso!"),
-      error: () => this.toastService.error("Erro inesperado! Tente novamente mais tarde")
-    })
+  submit() {
+    this.loginService
+      .login(this.loginForm.value.email, this.loginForm.value.password)
+      .subscribe({
+        next: () => {
+          this.toastService.success('Login feito com sucesso!');
+          this.router.navigate(['dashboard']);
+        },
+        error: () =>
+          this.toastService.error(
+            'Erro inesperado! Tente novamente mais tarde'
+          ),
+      });
   }
 
-  navigate(){
-    this.router.navigate(["signup"])
+  navigate() {
+    this.router.navigate(['signup']);
   }
 }
