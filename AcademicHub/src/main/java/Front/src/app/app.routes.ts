1e2b2912f1bb85af0d8cfd4204ed677b5efec555
0xPedrop <paulo.pedro@academico.ifpb.edu.br>
2025-02-10 20:49:14 -0300

    Criando componente Notifications e sua rota

--- AcademicHub/src/main/java/Front/src/app/app.routes.ts
+++ AcademicHub/src/main/java/Front/src/app/app.routes.ts
@@ -3,32 +3,37 @@ import { LoginComponent } from './pages/login/login.component';
 import { SignupComponent } from './pages/signup/signup.component';
 import { ProfileComponent } from './pages/profile/profile.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { NotificationsComponent } from './pages/notifications/notifications.component';
 
 export const routes: Routes = [
-    //criando rota de login
-    {
-        path: "",
-        pathMatch: "full",
-        redirectTo: "login"
-    },
-    {
-        path: "login",
-        component: LoginComponent
-    },
-    {
-        path: "signup",
-        component: SignupComponent
-    },
-    {
-        path: "dashboard",
-        component: DashboardComponent
-    },
-    {
-        path: "profile",
-        component: ProfileComponent
-    },
-    {
-        path: "**",
-        redirectTo: "login"
-    }
+  //criando rota de login
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'login',
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: 'signup',
+    component: SignupComponent,
+  },
+  {
+    path: 'dashboard',
+    component: DashboardComponent,
+  },
+  {
+    path: 'profile',
+    component: ProfileComponent,
+  },
+  {
+    path: 'notifications',
+    component: NotificationsComponent,
+  },
+  {
+    path: '**',
+    redirectTo: 'login',
+  },
 ];
