43c0e008116805dfadec950f3488bce63f973de7
0xJotave <joaovmb15@gmail.com>
2025-02-11 19:04:38 -0300

    Colocando sidebar e fazendo ajustes

--- AcademicHub/src/main/java/Front/src/app/pages/notifications/notifications.component.ts
+++ AcademicHub/src/main/java/Front/src/app/pages/notifications/notifications.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-notifications',
@@ -6,6 +6,18 @@ import { Component } from '@angular/core';
   templateUrl: './notifications.component.html',
   styleUrl: './notifications.component.scss'
 })
-export class NotificationsComponent {
+export class NotificationsComponent implements OnInit {
+  // Definindo os itens do Sidebar
+  menuItems = [
+    { name: 'Home', icon: 'assets/casa.png' },
+    { name: 'Search', icon: 'assets/procurar.png' },
+    { name: 'Notifications', icon: 'assets/sino-selecionado.png' },
+    { name: 'Saved Posts', icon: 'assets/marca-paginas.png' },
+    { name: 'Profile', icon: 'assets/do-utilizador.png' }
+  ];
 
+  constructor() { }
+
+  ngOnInit(): void {
+  }
 }
