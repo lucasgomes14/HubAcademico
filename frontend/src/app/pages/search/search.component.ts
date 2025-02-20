ed60798353629b1e0dc6fab02cc1bfce89b51353
Isaac Alves Leal de Oliveira <isaacalves2406@gmail.com>
2025-02-20 19:50:23 -0300

    Logica Search

--- frontend/src/app/pages/search/search.component.ts
+++ frontend/src/app/pages/search/search.component.ts
@@ -1,11 +1,31 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-search',
-  imports: [],
   templateUrl: './search.component.html',
-  styleUrl: './search.component.scss'
+  styleUrls: ['./search.component.css'],
+  imports: [FormsModule, CommonModule],
+  standalone:true
 })
 export class SearchComponent {
+  searchTerm: string = '';
+  usuarios: any[] = [];
 
+  constructor(private http: HttpClient) {}
+
+  buscarUsuarios() {
+    if (this.searchTerm.trim() === '') {
+      this.usuarios = [];
+      return;
+    }
+
+    this.http.get<any[]>(`http://localhost:8080/usuarios?nome=${this.searchTerm}`)
+      .subscribe(response => {
+        this.usuarios = response;
+      });
+  }
 }
